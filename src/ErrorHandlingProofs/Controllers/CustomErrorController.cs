d715fe44fc44b21bfbf603182f8dcaf1c6d26e0f
ghstahl@gmail.com <ghstahl@gmail.com>
2018-05-15 11:16:39 -0700

    initial

---
+++ src/ErrorHandlingProofs/Controllers/CustomErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ErrorHandlingProofs.Controllers
+{
+    public class CustomErrorController : Controller
+    {
+        public IActionResult Index(int? statusCode = null)
+        {
+            if (statusCode.HasValue)
+            {
+                if (statusCode.Value == 404 || statusCode.Value == 500)
+                {
+                    var viewName = statusCode.ToString();
+                    return View(viewName);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ src/ErrorHandlingProofs/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ErrorHandlingProofs.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public async Task<IActionResult> OnPostAsync(string button)
+        {
+            if (string.IsNullOrWhiteSpace(button))
+            {
+                throw new ArgumentNullException(nameof(button));
+            }
+
+            switch (button)
+            {
+                case "ApplicationException":
+                    throw new ApplicationException($"{button} Exception");
+                    break;
+                case "NotFoundResult":
+                    return new NotFoundResult();
+                    break;
+                case "StatusCodeResult-500":
+                    return new StatusCodeResult(500);
+                    break;
+                case "ArgumentNullException":
+                    throw new ArgumentNullException(nameof(button));
+                    break;
+                default:
+                    throw new Exception($"{button} is unknown!");
+            }
+ 
+
+            // If we got this far, something failed, redisplay form
+            return Page();
+        }
+    }
+}
